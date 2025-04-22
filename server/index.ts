56f3d5c86f783dab398cbb8fee5a00c2652ff79d
root <root@vmi2151629.contaboserver.net>
2025-04-22 20:59:27 +0200

    updates

--- server/index.ts
+++ server/index.ts
@@ -4,8 +4,15 @@ dotenv.config();
 import express, { type Request, Response, NextFunction } from "express";
 import { registerRoutes } from "./routes";
 import { setupVite, serveStatic, log } from "./vite";
+import cors from "cors";
 
 const app = express();
+
+app.use(cors({
+  origin: "http://45.90.120.55:8080",
+  credentials: true
+}));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
@@ -52,12 +59,7 @@ app.use((req, res, next) => {
 
   // importantly only setup vite in development and after
   // setting up all the other routes so the catch-all route
-  // doesn't interfere with the other routes
-  if (app.get("env") === "development") {
-    await setupVite(app, server);
-  } else {
-    serveStatic(app);
-  }
+  // doesn't interfere with the other rout
 
   // ALWAYS serve the app on port 5000
   // this serves both the API and the client.
